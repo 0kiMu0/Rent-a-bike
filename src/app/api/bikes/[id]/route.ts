565fdbad83337e7ecdef6da6d2da5b435996004b
Muhamed <muhammeddurmic@hotmail.com>
2025-05-07 22:23:06 +0200

    Delete second update

--- src/app/api/bikes/[id]/route.ts
+++ src/app/api/bikes/[id]/route.ts
@@ -1,14 +1,16 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/app/lib/db';
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-      const { id } = params; 
-      await pool.query('DELETE FROM bikes WHERE id = $1', [id]);
-      return new Response(JSON.stringify({ message: 'Bike deleted' }), { status: 200 });
-    } catch (err: unknown) {
-      const error = err as Error;
-      console.error('Greška u DELETE /api/bikes:', error.message);
-      return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-    }
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  try {
+    const { id } = context.params;
+
+    await pool.query('DELETE FROM bikes WHERE id = $1', [id]);
+
+    return NextResponse.json({ message: 'Bike deleted' }, { status: 200 });
+  } catch (err: unknown) {
+    const error = err as Error;
+    console.error('Greška u DELETE /api/bikes/[id]:', error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
+}
