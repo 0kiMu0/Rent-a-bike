b6aa939ab7353fec918fd40e348e733f7fb6a67c
Muhamed <muhammeddurmic@hotmail.com>
2025-05-07 22:28:36 +0200

    Delete third update

--- src/app/api/bikes/[id]/route.ts
+++ src/app/api/bikes/[id]/route.ts
@@ -1,12 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/app/lib/db';
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
-    const { id } = context.params;
-
+    const { id } = params;
     await pool.query('DELETE FROM bikes WHERE id = $1', [id]);
-
     return NextResponse.json({ message: 'Bike deleted' }, { status: 200 });
   } catch (err: unknown) {
     const error = err as Error;
